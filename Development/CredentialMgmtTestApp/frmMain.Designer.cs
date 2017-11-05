80b9aaebcf9c4edbc70b73b6eb8d61a5ada1ddf3
mylearnsource <33397908+mylearnsource@users.noreply.github.com>
2017-11-05 22:04:44 +0530

    Add files via upload

---
+++ Development/CredentialMgmtTestApp/frmMain.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace CredentialMgmtTestApp
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtUserName = new System.Windows.Forms.TextBox();
+            this.txtDomainKey = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.btnExistCheck = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.gvCredentials = new System.Windows.Forms.DataGridView();
+            this.btnLoadAll = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.gvCredentials)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtUserName
+            // 
+            this.txtUserName.Location = new System.Drawing.Point(86, 20);
+            this.txtUserName.MaxLength = 20;
+            this.txtUserName.Name = "txtUserName";
+            this.txtUserName.Size = new System.Drawing.Size(159, 20);
+            this.txtUserName.TabIndex = 0;
+            // 
+            // txtDomainKey
+            // 
+            this.txtDomainKey.Location = new System.Drawing.Point(86, 76);
+            this.txtDomainKey.MaxLength = 20;
+            this.txtDomainKey.Name = "txtDomainKey";
+            this.txtDomainKey.Size = new System.Drawing.Size(159, 20);
+            this.txtDomainKey.TabIndex = 2;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(86, 48);
+            this.txtPassword.MaxLength = 20;
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(159, 20);
+            this.txtPassword.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(17, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "User Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(17, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Password";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(17, 80);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(66, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Domain/Key";
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(86, 131);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(77, 23);
+            this.btnSave.TabIndex = 4;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.Location = new System.Drawing.Point(169, 131);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(75, 23);
+            this.btnRemove.TabIndex = 5;
+            this.btnRemove.Text = "Remove";
+            this.btnRemove.UseVisualStyleBackColor = true;
+            this.btnRemove.Click += new System.EventHandler(this.btnRemove_Click);
+            // 
+            // btnExistCheck
+            // 
+            this.btnExistCheck.Location = new System.Drawing.Point(86, 102);
+            this.btnExistCheck.Name = "btnExistCheck";
+            this.btnExistCheck.Size = new System.Drawing.Size(77, 23);
+            this.btnExistCheck.TabIndex = 3;
+            this.btnExistCheck.Text = "Exist Check";
+            this.btnExistCheck.UseVisualStyleBackColor = true;
+            this.btnExistCheck.Click += new System.EventHandler(this.btnExistCheck_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(251, 23);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(124, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "List of Saved Credentials";
+            // 
+            // gvCredentials
+            // 
+            this.gvCredentials.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gvCredentials.Location = new System.Drawing.Point(254, 40);
+            this.gvCredentials.Name = "gvCredentials";
+            this.gvCredentials.Size = new System.Drawing.Size(696, 285);
+            this.gvCredentials.TabIndex = 9;
+            // 
+            // btnLoadAll
+            // 
+            this.btnLoadAll.Location = new System.Drawing.Point(86, 160);
+            this.btnLoadAll.Name = "btnLoadAll";
+            this.btnLoadAll.Size = new System.Drawing.Size(159, 23);
+            this.btnLoadAll.TabIndex = 10;
+            this.btnLoadAll.Text = "Load All";
+            this.btnLoadAll.UseVisualStyleBackColor = true;
+            this.btnLoadAll.Click += new System.EventHandler(this.btnLoadAll_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(169, 102);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 11;
+            this.btnClear.Text = "Clear Fields";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(955, 328);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnLoadAll);
+            this.Controls.Add(this.gvCredentials);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnExistCheck);
+            this.Controls.Add(this.btnRemove);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtDomainKey);
+            this.Controls.Add(this.txtUserName);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Credential Management Test";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.gvCredentials)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtUserName;
+        private System.Windows.Forms.TextBox txtDomainKey;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnRemove;
+        private System.Windows.Forms.Button btnExistCheck;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DataGridView gvCredentials;
+        private System.Windows.Forms.Button btnLoadAll;
+        private System.Windows.Forms.Button btnClear;
+    }
+}
+
---
+++ Development/CredentialMgmtTestApp/frmMain.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CredentialManagement;
+
+namespace CredentialMgmtTestApp
+{
+    public partial class frmMain : Form
+    {
+        CredentialManagement.Credential objCre;
+
+        public frmMain()
+        {
+            InitializeComponent();
+           
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            objCre = new Credential();
+
+            objCre.Username = txtUserName.Text.Trim();
+            objCre.Password = txtPassword.Text.Trim();
+            objCre.Target = txtDomainKey.Text.Trim();
+
+            if (!ControlsValidate())
+            {
+                MessageBox.Show("Enter all fields");
+            }
+            else
+            {
+                if (objCre.Exists())
+                {
+                    MessageBox.Show("Sorry, Already Exists!");
+                }
+                else
+                {
+                    objCre.Save();
+                    MessageBox.Show("Saved!");
+                    ClearFields();
+                    LoadCredential(false);
+                   
+                }
+            }
+        }
+
+        private bool ControlsValidate()
+        {
+            if (string.IsNullOrEmpty(txtUserName.Text.Trim()) || string.IsNullOrEmpty(txtDomainKey.Text.Trim()) || string.IsNullOrEmpty(txtPassword.Text.Trim()))
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        private void ClearFields()
+        {
+            txtDomainKey.Text = null;
+            txtPassword.Text = null;
+            txtUserName.Text = null;
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            LoadCredential(false);
+        }
+
+        private void btnExistCheck_Click(object sender, EventArgs e)
+        {
+            objCre = new Credential();
+
+            objCre.Username = txtUserName.Text.Trim();
+            objCre.Password = txtPassword.Text.Trim();
+            objCre.Target = txtDomainKey.Text.Trim();
+
+            if (!ControlsValidate())
+            {
+                MessageBox.Show("Enter all fields");
+            }
+            else
+            {
+                if(objCre.Exists())
+                {
+                    MessageBox.Show("Already Exists!");
+                }
+                else
+                {
+                    MessageBox.Show("Congrats, Available!");
+                }
+
+            }
+        }
+
+        private void btnRemove_Click(object sender, EventArgs e)
+        {
+
+            //Note: Delete only  PersistanceType = Session
+            CredentialSet set = new CredentialSet();
+            var credentialList = set.Load();
+            var FilteredData = credentialList.Where(wh => wh.Target == txtDomainKey.Text.Trim() && wh.PersistanceType == PersistanceType.Session).ToList();
+
+            if (FilteredData.Count > 0)
+            {
+                foreach (var item in FilteredData)
+                {
+                    item.Delete();
+                }
+                MessageBox.Show("Item Deleted!");
+                LoadCredential(false);
+            }
+            else
+            {
+                MessageBox.Show("Enter Domain");
+            }
+        }
+
+        
+
+        private void btnLoadAll_Click(object sender, EventArgs e)
+        {
+
+            LoadCredential(false);
+        }
+
+        private void LoadCredential(bool IsFiltered = true)
+        {
+            CredentialSet set = new CredentialSet();
+            var credentialList = set.Load();
+
+            if(!IsFiltered)
+            {
+                txtDomainKey.Text = "";
+            }
+
+            if (string.IsNullOrEmpty(txtDomainKey.Text.Trim()))
+            {
+                var FilteredData = credentialList.ToList();
+
+                gvCredentials.DataSource = FilteredData;
+            }
+            else
+            {
+                var FilteredData = credentialList.Where(wh => wh.Target == txtDomainKey.Text.Trim()).ToList();
+
+                gvCredentials.DataSource = FilteredData;
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            ClearFields();
+
+            MessageBox.Show("Cleared!");
+        }
+    }
+
+}
